fe2bb287d399cade7e6bad28f84b3e754355bfba
egorsv1985 <egorvs2009@gmail.com>
2022-04-07 16:00:03 +0300

    header-375, 1024

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -3,18 +3,19 @@ export function isWebp() {
 
 		var webP = new Image();
 		webP.onload = webP.onerror = function () {
-		callback(webP.height == 2);
+			callback(webP.height == 2);
 		};
 		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-		}
-		
-		testWebP(function (support) {
-		
+	}
+
+	testWebP(function (support) {
+
 		if (support == true) {
-		document.querySelector('body').classList.add('webp');
-		}else{
-		document.querySelector('body').classList.add('no-webp');
+			document.querySelector('body').classList.add('webp');
+		} else {
+			document.querySelector('body').classList.add('no-webp');
 		}
-		});
+	});
 }
 
+export function customSelect() {}
